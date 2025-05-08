df8c4ce4d8ee88f74dff008876b4ee3dff6140bc
canhtran <canhtran@virtuosgames.com>
2025-05-08 14:04:24 +0700

    update addons version

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
 	"name": "HardSurface Tool",
 	"description": "This add-on provides a set of custom tools for working efficiently in Blender.",
 	"author": "Canh Tran",
-	"version": (1, 0, 4),
+	"version": (1, 0, 5),
 	"blender": (2, 80, 0),
 	"location": "View3D",
 	"category": "3D View"
